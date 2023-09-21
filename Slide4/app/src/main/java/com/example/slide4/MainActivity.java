395b36e88effe69147c3e328a6d4932aea8e54fd
Shiro Hien <thaohien0607@gmail.com>
2023-09-21 07:51:03 +0700

    Add up to slide 6

---
+++ Slide4/app/src/main/java/com/example/slide4/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.slide4;
+
+import static android.view.inputmethod.EditorInfo.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nameEditText;
+    private EditText phoneEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        //EditText
+        nameEditText = findViewById(R.id.name_EditText);
+        phoneEditText = findViewById(R.id.phone_EditText);
+        nameEditText.setOnEditorActionListener((v, actionID, event) -> {
+            if (actionID == IME_ACTION_DONE || actionID == IME_ACTION_NEXT) {
+                InputMethodManager i = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                i.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+        phoneEditText.setOnEditorActionListener((v, actionID, event) -> {
+            if (actionID == IME_ACTION_DONE || actionID == IME_ACTION_NEXT) {
+                InputMethodManager i = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                i.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+    }
+}
